fcebfcdaee45d436408313493805e152178f1f87
Vinicius Silva <106930436+vinirex@users.noreply.github.com>
2023-11-27 21:22:14 -0300

    login page

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const btnLogin = document.querySelector('#btn');
+const prontuario = document.querySelector('#med-info');
+
+btnLogin.addEventListener('click', userAuth())
+
+function userAuth(){
+    console.log('oiiiii')
+}
