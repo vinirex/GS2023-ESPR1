83afc75c14f702c01c99523802f9708cd4970a46
Vinicius Silva <106930436+vinirex@users.noreply.github.com>
2023-11-28 19:40:34 -0300

    js start

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,37 @@
 const btnLogin = document.querySelector('#btn');
 const prontuario = document.querySelector('#med-info');
 
-btnLogin.addEventListener('click', userAuth())
+btnLogin.addEventListener('click', userAuth)
 
 function userAuth(){
-    console.log('oiiiii')
+    console.log('Howdy');
+    let user = document.getElementById(nome).value;
+    let password = document.getElementById(password).value;
+
+    for (let i = 0; i < users.length; i++){
+        prontuario.innerHTML += ``
+    }
+
 }
+
+
+const users = [
+    {
+      name:"Dr.Roberto Ghenji",
+      user:"Robertomed",
+      senha: "test@1234",
+      adm: true
+    },
+      {
+      name:"Maria Aparecida",
+      user:"Maria",
+      senha:"test@1234",
+      adm:false,
+      },
+      {
+      name:"Antonio Alberto",
+      user:"Antonio",
+      senha:"test@1234",
+      adm:false,
+      }
+    ]
